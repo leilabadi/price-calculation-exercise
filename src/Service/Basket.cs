4cd18fe1f0d5795d85277d4ae80b3c1732fb9be4
Mostafa <leilabadi@gmail.com>
2021-12-06 01:44:58 +0000

    implemented the bag logic and updated the tests to use the new bag implementation instead of mock

---
+++ src/Service/Basket.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using PriceCalculationExercise.Contracts;
+using PriceCalculationExercise.Contracts.Offer;
+using PriceCalculationExercise.Domain;
+
+namespace PriceCalculationExercise.Service
+{
+    public class Basket : IBasket
+    {
+        public ICustomer Customer { get; }
+
+        private readonly List<IBasketItem> items;
+        public IReadOnlyList<IBasketItem> Items => items;
+
+        private readonly List<IDiscount> discounts;
+        public IReadOnlyList<IDiscount> Discounts => discounts;
+
+        public Basket(ICustomer customer, List<IDiscount> discounts)
+        {
+            Customer = customer;
+            this.items = new List<IBasketItem>();
+            this.discounts = discounts;
+        }
+
+        public void AddProduct(IProduct product, int quantity)
+        {
+            items.Add(new BasketItem(new Product(product), quantity));
+        }
+    }
+}
--- test/IntegrationTests/PriceCalculationTests.cs
+++ test/IntegrationTests/PriceCalculationTests.cs
@@ -13,16 +13,15 @@ namespace PriceCalculationExercise.IntegrationTests
 {
     public class PriceCalculationTests
     {
+        private readonly Mock<ICustomer> customerMock;
+        private readonly List<IDiscount> discounts;
         private readonly IProduct productBread;
         private readonly IProduct productButter;
         private readonly IProduct productMilk;
-        private readonly List<IDiscount> discounts;
 
         public PriceCalculationTests()
         {
-            productBread = new Product("Bread", 1.00m);
-            productButter = new Product("Butter", 0.80m);
-            productMilk = new Product("Milk", 1.15m);
+            customerMock = new Mock<ICustomer>();
 
             discounts = new IDiscount[]
             {
@@ -35,27 +34,25 @@ namespace PriceCalculationExercise.IntegrationTests
                     new PercentageOffOutcome(100),
                     new ItemTarget(productMilk))
             }.ToList();
+
+            productBread = new Product("Bread", 1.00m);
+            productButter = new Product("Butter", 0.80m);
+            productMilk = new Product("Milk", 1.15m);
         }
 
         [Fact]
         public void Should_not_apply_discount_when_basket_does_not_qualify_for_any_discounts()
         {
             // Arrange
-            var basketItems = new IBasketItem[]
-            {
-                new BasketItem(new Product(productBread), 1),
-                new BasketItem(new Product(productButter), 1),
-                new BasketItem(new Product(productMilk), 1)
-            }.ToList();
-
-            var basket = new Mock<IBasket>();
-            basket.Setup(p => p.Items).Returns(basketItems);
-            basket.Setup(p => p.Discounts).Returns(discounts);
+            var basket = new Basket(customerMock.Object, discounts);
+            basket.AddProduct(new Product(productBread), 1);
+            basket.AddProduct(new Product(productButter), 1);
+            basket.AddProduct(new Product(productMilk), 1);
 
             var sut = new PriceCalculator();
 
             // Act
-            var total = sut.CalculateTotalCost(basket.Object);
+            var total = sut.CalculateTotalCost(basket);
 
             // Assert
             total.Should().Be(2.95m);
@@ -65,20 +62,14 @@ namespace PriceCalculationExercise.IntegrationTests
         public void Should_apply_half_price_bread_discount_when_basket_has_two_butter()
         {
             // Arrange
-            var basketItems = new IBasketItem[]
-            {
-                new BasketItem(new Product(productBread), 2),
-                new BasketItem(new Product(productButter), 2)
-            }.ToList();
-
-            var basket = new Mock<IBasket>();
-            basket.Setup(p => p.Items).Returns(basketItems);
-            basket.Setup(p => p.Discounts).Returns(discounts);
+            var basket = new Basket(customerMock.Object, discounts);
+            basket.AddProduct(new Product(productBread), 2);
+            basket.AddProduct(new Product(productButter), 2);
 
             var sut = new PriceCalculator();
 
             // Act
-            var total = sut.CalculateTotalCost(basket.Object);
+            var total = sut.CalculateTotalCost(basket);
 
             // Assert
             total.Should().Be(3.10m);
@@ -88,19 +79,13 @@ namespace PriceCalculationExercise.IntegrationTests
         public void Should_apply_free_milk_discount_when_basket_has_four_milk()
         {
             // Arrange
-            var basketItems = new IBasketItem[]
-            {
-                new BasketItem(new Product(productMilk), 4)
-            }.ToList();
-
-            var basket = new Mock<IBasket>();
-            basket.Setup(p => p.Items).Returns(basketItems);
-            basket.Setup(p => p.Discounts).Returns(discounts);
+            var basket = new Basket(customerMock.Object, discounts);
+            basket.AddProduct(new Product(productMilk), 4);
 
             var sut = new PriceCalculator();
 
             // Act
-            var total = sut.CalculateTotalCost(basket.Object);
+            var total = sut.CalculateTotalCost(basket);
 
             // Assert
             total.Should().Be(3.45m);
@@ -110,21 +95,15 @@ namespace PriceCalculationExercise.IntegrationTests
         public void Should_apply_free_milk_and_half_price_bread_discount_when_basket_has_eight_milk_and_two_butter()
         {
             // Arrange
-            var basketItems = new IBasketItem[]
-            {
-                new BasketItem(new Product(productBread), 1),
-                new BasketItem(new Product(productButter), 2),
-                new BasketItem(new Product(productMilk), 8)
-            }.ToList();
-
-            var basket = new Mock<IBasket>();
-            basket.Setup(p => p.Items).Returns(basketItems);
-            basket.Setup(p => p.Discounts).Returns(discounts);
+            var basket = new Basket(customerMock.Object, discounts);
+            basket.AddProduct(new Product(productBread), 1);
+            basket.AddProduct(new Product(productButter), 2);
+            basket.AddProduct(new Product(productMilk), 8);
 
             var sut = new PriceCalculator();
 
             // Act
-            var total = sut.CalculateTotalCost(basket.Object);
+            var total = sut.CalculateTotalCost(basket);
 
             // Assert
             total.Should().Be(9.00m);
